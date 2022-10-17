53e1fe7d7cb7a874275d44e22941492d53ce7c6a
Warlera <90709624+Warlera@users.noreply.github.com>
2022-10-17 22:25:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isVisible){ //если кнопка показана 
+		tg.MainButton.hide() //скрываем кнопку 
+	}
+  else{ //иначе
+  	tg.MainButton.show() //показываем 
+  }
+});
